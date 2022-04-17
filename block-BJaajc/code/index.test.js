b02e192f376d4b2c34971bf39a557dd34b2df200
umakant-2113 <lodhiumakant800@gmail.com>
2022-04-17 12:33:23 +0530

    done

--- block-BJaajc/code/index.test.js
+++ block-BJaajc/code/index.test.js
@@ -43,6 +43,8 @@ test(`isPalindrome palinDrome toBeTruthy palinDrome `,()=>{
     expect( isPalindrome("palinDrome") ).toBeTruthy();
 })
 
+
+
 test(`isPalindrome palindrome toBeTruthy palindrome`,()=>{
     expect(isPalindrome("rajput")).toBeTruthy();
 })
@@ -56,7 +58,7 @@ expect(  getCircumfrence(7) ).toMatch(`44`)
 test(`getArea 7 to equal 44 `,()=>{
     expect(  getArea(7) ).toMatch(`44`)
     } )
-    
+
 // negative test
 
 test(`getCircumfrence 7 to equal 44 `,()=>{
