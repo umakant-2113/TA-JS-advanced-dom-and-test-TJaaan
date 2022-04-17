125bd9997032c3b844c88af93451328ca391d180
umakant-2113 <lodhiumakant800@gmail.com>
2022-04-17 12:15:53 +0530

    last assignment advance dom

---
+++ block-BJaajc/code/index.js
@@ -0,0 +1,32 @@
+
+
+
+
+function add(a,b){
+return a+b
+}
+
+
+
+function fullName(firstName,lastName){
+return firstName+lastName;
+}
+
+
+function isPalindrome(value){
+if(value==="palinDrome"){
+return true;
+}else{
+    return false;
+}
+}
+
+function getCircumfrence(radius){
+    return ` The circumference is ${2*22/7*radius}`  
+}
+
+function getArea(radius){
+return `The area is ${22/7*radius*radius}`
+}
+
+module.exports = {add, fullName,isPalindrome,getCircumfrence,getArea}
---
+++ block-BJaajc/code/index.test.js
@@ -0,0 +1,55 @@
+const {add,fullName,isPalindrome,getCircumfrence,getArea} =  require('./index');
+
+// positive test 
+test('add 1+2 to equal 3',()=>{
+    expect(add(1,2)).toBe(3);
+})
+
+test('add 9+8 to equal 17',()=>{
+    expect(add(9,8)).toBe(17);
+})
+test('add 11+9 to equal 20',()=>{
+    expect(add(11,9)).toBe(20);
+})
+
+// negative test 
+
+
+test('add 9+12 to equal 21',()=>{
+    expect(add(9,12)).toBe(2);
+})
+
+
+test(`add 7+5 to equal 15`,()=>{
+expect(add(7,5)).toBe(11);
+})
+
+test(`add 7+5 to equal 15`,()=>{
+    expect(add(7,5)).toBe(11);
+})
+
+
+test(`fullName raj+kappor match to rajkappor`, ()=>{
+    expect(fullName('raj','kappor')).toMatch('rajkappor')
+})
+
+test(`fullName radha+rajput match to radharajput`,()=>{
+    expect(fullName("radha","rajput")).toMatch("radha rajput")
+})
+
+
+test(`isPalindrome palinDrome toBeTruthy palinDrome `,()=>{
+    expect( isPalindrome("palinDrome") ).toBeTruthy();
+})
+
+test(`isPalindrome palindrome toBeTruthy palindrome`,()=>{
+    expect(isPalindrome("rajput")).toBeTruthy();
+})
+
+test(`getCircumfrence 7 to equal 44 `,()=>{
+expect(  getCircumfrence(7) ).toMatch(`44`)
+})
+
+test(`getArea 7 to equal 44 `,()=>{
+    expect(  getArea(7) ).toMatch(`44`)
+    } )
